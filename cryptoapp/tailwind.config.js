1eb9766bcaeb6747e1f9274d2e87e8d39c440541
Jason MR <jbmarroq@gmail.com>
2024-05-02 23:26:35 +1000

    updated path to Components with capiital C

--- cryptoapp/tailwind.config.js
+++ cryptoapp/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
+    "./Components/**/*.{js,jsx}",
     "./app/**/*.{js,jsx}",
     "./src/**/*.{js,jsx}",
   ],
