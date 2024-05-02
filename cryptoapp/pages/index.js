3b6edbc0cb9450c5297a79598b691986a1ea8dc7
Jason MR <jbmarroq@gmail.com>
2024-05-02 23:13:29 +1000

    changed intro for testing staging

--- cryptoapp/pages/index.js
+++ cryptoapp/pages/index.js
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Welcome to CryptoGuru</h1>
-      <p>Get rich now!!</p>
+      <p>Try it now!!</p>
       <Link href={`/coins`}>Go to were the action is💰💰💰</Link>
     </div>
   );
