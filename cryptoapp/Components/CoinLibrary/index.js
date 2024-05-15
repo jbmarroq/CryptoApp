e6ed36cc7293f311801745028134c9ea6b4a38b2
Jason MR <jbmarroq@gmail.com>
2024-05-15 21:42:46 +1000

    polished navbar

--- cryptoapp/Components/CoinLibrary/index.js
+++ cryptoapp/Components/CoinLibrary/index.js
@@ -20,20 +20,18 @@ import { MenuBar } from "../Menubar";
 export function CoinLibrary({}) {
   return (
     <>
-      <div className="sticky top-0 z-50 w-full border-transparent bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60  flex flex-wrap items-center justify-between dark:border-transparent p-4 md:px-6">
+      <div className="sticky top-0 z-50 w-full border-transparent bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60  flex flex-wrap items-center justify-between dark:border-transparent md:px-6">
         <NavMenu />
         <MenuBar />
         <ModeToggle />
       </div>
       {/* </div> */}
+      <h1 className="scroll-m-20 border-b pb-2  ml-0 md:ml-4 text-3xl font-semibold tracking-tight first:mt-0 dark:border-stone-700">
+        Trending Coins
+      </h1>
 
+      <EmblaCarousel />
       <div className="max-w-7xl mx-auto px-0 sm:px-0 lg:px-8">
-        <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 dark:border-stone-700">
-          Trending Coins
-        </h1>
-
-        <EmblaCarousel />
-
         <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 dark:border-stone-700 text-center p-10">
           Cryptocurrency Prices by Market Cap
         </h1>
